196db03843d02e99c5aa77689548d6d4d258f43e
AyanDaw <official.ayan.daw@gmail.com>
2025-11-27 23:22:34 +0530

    Create Roman_to_Integer.py

---
+++ 13_Roman_to_Integer/Roman_to_Integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dc = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000} # Making a Dictionary first
+        inte = 0                                                               # A variable where i shall store the value
+        i = 0                                                                  # A counter variable for loop
+        n = len(s)                                                             # measuring the lenth of s
+
+        while i < n:                                                           # Making sure the counter wont get past of the available index
+            if i + 1 < n and (dc[s[i]] < dc[s[i + 1]]):                        # Checking if next digit is bigger or not. like IV, IX etc.
+                inte += dc[s[i + 1]] - dc[s[i]]                                # If true then we will subtract and then add the rest
+                i += 2                                                         # Ofcourse the next one is already checked so we wont check the next one instead we will skip that
+            else:
+                inte += dc[s[i]]                                               # If false then we will follow the normal path
+                i += 1                                                         # Then as usual we will check the next one
+
+        return inte                                                            # Returning the integer value
