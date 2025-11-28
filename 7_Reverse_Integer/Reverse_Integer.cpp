d622cb4b20f075beb4c1af32ad7b2127205c225c
AyanDaw <official.ayan.daw@gmail.com>
2025-11-28 20:26:09 +0530

    Create Reverse_Integer.cpp

---
+++ 7_Reverse_Integer/Reverse_Integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long r = 0; // While intermidiate the value might exceed 32-bit signed value. As we are said "Assume the environment does not allow you to store 64-bit integers (signed or unsigned)." thats why we are using Long long.
+        while(x != 0){// It does not matter the number is Positive or Negative, until the number is 0.
+            r = r*10 + x%10; // Normal reverse algorithm
+            x /= 10;
+        }
+        if (x <= INT_MAX && x >= INT_MIN) // -2^31 <= x <= 2^31 - 1. INT_MAX and INT_MIN are already predefined in the macro. I checked.
+            return (int)r; // Type casting into Integer
+        else
+            return 0; // As conditioned.
+    }
+};
