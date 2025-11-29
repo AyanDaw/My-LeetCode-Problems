a2278ce7c9e58f875cb7f5f865d5f60c1efd46ad
AyanDaw <official.ayan.daw@gmail.com>
2025-11-30 00:44:21 +0530

    Update First_Missing_Positive.py

--- 0041_First_Missing_Positive/First_Missing_Positive.py
+++ 0041_First_Missing_Positive/First_Missing_Positive.py
@@ -1,17 +1,19 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n = len(nums)       # Getting length of List
-        i = 0               # Counter
+        n = len(nums)  # Getting length of List
+        i = 0  # Counter
 
         while i < n:
-            correctindex = nums[i] - 1      # Unlike C++, Pyhton is not that much intelligent to calculate inside the List 3rd brackets
-            if 0 < nums[i] < n+1 and nums[i] != nums[correctindex]:
-                nums[i], nums[correctindex] = nums[correctindex] , nums[i]      # Swap
+            # Unlike C++, Pyhton is not that much intelligent to calculate inside the List 3rd brackets
+            correctindex = nums[i] - 1
+            if 0 < nums[i] < n + 1 and nums[i] != nums[correctindex]:
+                nums[i], nums[correctindex] = nums[correctindex], nums[i]  # Swap
             else:
-                i += 1      # Increment
-            
+                i += 1  # Increment
+
         for i in range(n):
-            if nums[i] != i+1:      # At which element we could not find the next index same that one is the missing
-                return i + 1        # Returning 
-        
-        return n + 1        # If 1...n all are available then n+1 is the missing one
+            # At which element we could not find the next index same that one is the missing
+            if nums[i] != i + 1:
+                return i + 1  # Returning
+
+        return n + 1  # If 1...n all are available then n+1 is the missing one
