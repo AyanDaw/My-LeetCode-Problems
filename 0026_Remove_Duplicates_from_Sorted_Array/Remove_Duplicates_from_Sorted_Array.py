b8c0c89128c014cbeb4732666d384495528990b6
AyanDaw <official.ayan.daw@gmail.com>
2025-12-03 23:02:11 +0530

    Update Remove_Duplicates_from_Sorted_Array.py

--- 0026_Remove_Duplicates_from_Sorted_Array/Remove_Duplicates_from_Sorted_Array.py
+++ 0026_Remove_Duplicates_from_Sorted_Array/Remove_Duplicates_from_Sorted_Array.py
@@ -1,14 +1,16 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        n = len(nums) 
-        prev = nums[0]
-        i = 1
-        k = 1
-        for i in nums[1:]:
-            
-            if prev != i:
-                prev = i
+        if not nums:
+            return 0
+
+        prev = nums[0]  # Taking the first element
+        k = 1  # Unique value counter. First one will always be unique so 1
+        for i in nums[1:]:  # Taking values from 1 index, not 0.
+
+            if prev != i:  # If value doesnot match with previous element, then it means it's unique
+                prev = i  # Updating the previous value variable
+                # Inserting the current element after the last unique value
                 nums[k] = i
-                k += 1
-                
-        return k
+                k += 1  # Increasing counter by 1.
+
+        return k  # Returning the counter value.
