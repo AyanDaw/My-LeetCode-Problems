75b14ce27561cd9e0a404efcb4badf80bb7f9171
AyanDaw <official.ayan.daw@gmail.com>
2025-12-06 00:38:10 +0530

    3sum.py

--- 0015_3Sum/3Sum.cpp
+++ 0015_3Sum/3Sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans;     // List of Lists er variable, which will be returned
         int n = nums.size();
-        sort(nums.begin(),nums.end());
+
+        // Sorting the array so we can implement teo pointers and it will reduce the time if we use sort for every possible triplets and create duplicates too
+        sort(nums.begin(),nums.end());   
         for (int i=0; i<n; i++){
             
-            if (i>0 && nums[i]==nums[i-1]) continue;
+            if (i>0 && nums[i]==nums[i-1]) continue; // If the next element's value is the same as previous element then we will skip cause the list is already sorted.
             
-            int j = i+1;
-            int k = n-1;
-            while(j < k){
+            int j = i+1; // next element of i. So ith index can stay != j
+            int k = n-1; // end point of the array
+            while(j < k){ // Until the j and k clashes
                 int zero = nums[i] + nums[j] + nums[k];
-                if(zero < 0){
-                    //tor ans negative beroche mane toke ans take ektu kom negative banate hobe so amra dandike jabo, j++
+                if(zero < 0){ // If the ans becomes -ve
+                    // Your ans is negative that means we have to make the ans less negative so we will move the j towards right means j++
                     j++;
-                    // ans negative berolo
                 }
-                else if(zero > 0){
-                    //tor ans positive beroche mane toke ans take ektu kom positive banate hobe so amra baike jabo, k--
+                else if(zero > 0){ // If the ans is +ve
+                    // Your ans is positive that means we have to make the ans less positive so we will move the k towards left means k--
                     k--;
-                    // ans positive berolo
                 }
-                else{
-                    vector<int> temp = {nums[i], nums[j], nums[k]};// ans 0 holo
-                    //Already sorted r duplicate check hoye ache so chinta nei
-                    ans.push_back(temp); // Pechon theke push hoche
-                    j++;
-                    k--; // porer ta check korar jonno ready
-                    // But eki values abar ele tokhun duplicate hobe, so jotokhun na value change hoche barate thakbo, And until they cross each other
+                else{ // If the ans gives 0
+                    vector<int> temp = {nums[i], nums[j], nums[k]}; // saving the elements in array form in a array.
+                    // This list is already sorted and no duplicate so no overthink
+                    ans.push_back(temp); // Enters new triplets at the end
+                    j++; 
+                    k--; // Readying them for next element
+                    // But if the same values comes again this will make duplicates. so untill the value changes we will keep increasing it, And until they cross each other
                     // mind it i am talking about same value not same element
                     while((j < k) && nums[j]==nums[j-1]) j++;
                     while((j < k) && nums[k]==nums[k+1]) k--;
