fd750cb420cb31c603274a30fe074db7e02b4a36
AyanDaw <official.ayan.daw@gmail.com>
2025-12-10 23:01:03 +0530

    Create Generate_Parentheses.py

---
+++ 0022_Generate_Parentheses/Generate_Parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n==0:
+            return [""]
+
+        res = []
+        def paras(current, o, c):
+            if len(current) == 2 * n:
+                res.append(current)
+                return
+            
+            if o < n:
+                paras(current + "(", o + 1, c)
+
+            if c < o:
+                paras(current + ")", o, c + 1)
+        
+        paras("",0,0)
+        return res
