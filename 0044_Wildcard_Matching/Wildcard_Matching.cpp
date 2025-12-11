00f3507e716b5ac0d7eaca0cfcab7c92f944bf38
AyanDaw <official.ayan.daw@gmail.com>
2025-12-11 21:53:48 +0530

    Create Wildcard_Matching.cpp

---
+++ 0044_Wildcard_Matching/Wildcard_Matching.cpp
@@ -0,0 +1,41 @@
+// Hard?? It was far easier than 10._regular_expression_Matching/
+
+class Solution {
+public:
+    bool f(vector<vector<int>> &dp, int i, int j, string &p, string &s){
+        // Base cases
+        if(i<0 && j<0) return true;
+        if(i<0 && j>=0) return false;
+        if(i>=0 && j<0){
+            for(int ii=i; ii>=0; ii--)
+            if(p[ii] != '*') return false;
+            return true;
+        }
+        if (dp[i][j] != -1) return dp[i][j];
+
+        // direct match or '?'
+        if (p[i] == s[j] || p[i] == '?') {
+            return dp[i][j] = f(dp, i-1, j-1, p, s);
+        }
+        if(p[i] == '*'){
+            return dp[i][j] = f(dp, i-1, j, p, s) || f(dp, i, j-1, p, s);
+        }
+
+        return dp[i][j] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+        if(m == 0 && n == 0) return true; 
+        // Dutoi faka.
+        
+        if(n==0) return false; 
+        // Text ache but pattern e kichu nei
+        
+        vector<vector<int>> dp(n, vector<int>(m, -1)); 
+        // Similar to 2D array. Every room is initialized with -1.
+
+        return f(dp, n-1, m-1, p, s);
+    }
+};
