608bb092efdc6dc8e9a0113bb7df1f3c08adb661
AyanDaw <official.ayan.daw@gmail.com>
2025-12-18 21:34:37 +0530

    Create Valid_Parentheses.py

---
+++ 0020_Valid_Parentheses/Valid_Parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        paras = { ")":"(", "}":"{", "]":"["} # I will be asking for opening brackets so those will be in values (check in line 10), and so closes will be in keys.
+        for ch in s:
+            if ch in paras.values():
+                stack.append(ch)
+
+            else:
+                if not stack or stack[-1] != paras[ch]:
+                    # Not stack means Stack khali? yes!
+                    # open na match korle true debe
+                    # total condition true hole False trigger korbe 
+                    return False
+                stack.pop()
+
+        if not stack:
+            # Stack khali, tarmane extra kichu pore nei tai true
+            return True
+        else:
+            return False
